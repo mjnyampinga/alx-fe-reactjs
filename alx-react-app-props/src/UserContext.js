ac14754ea9b7f58f9214b917c7cae45c3c3ad238
nmjoselyne09 <nmjoselyne09@gmail.com>
2025-11-06 03:08:14 +0200

    my-company

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,6 +1,10 @@
 // src/UserContext.js
 import React from "react";
 
-const UserContext = React.createContext();
+// create the context with a default shape
+const UserContext = React.createContext({
+  name: "",
+  email: "",
+});
 
 export default UserContext;
