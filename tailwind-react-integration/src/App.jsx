032a3129eb9c3dd83435913c2db0092c31fff64f
nmjoselyne09 <nmjoselyne09@gmail.com>
2025-11-26 11:21:30 +0200

    initial commit

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-3xl font-bold text-white">
+        Tailwind is working! 🎧
+      </h1>
+    </div>
+  );
+}
+
+export default App;
