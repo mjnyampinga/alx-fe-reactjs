9702b5c94bc60dc116d70725f9623011c5a965d7
nmjoselyne09 <nmjoselyne09@gmail.com>
2025-11-26 11:36:34 +0200

    new file

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,9 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-3xl font-bold text-white">
-        Tailwind is working! 🎧
-      </h1>
+    <div className="min-h-screen flex items-center justify-center bg-slate-200">
+      <UserProfile />
     </div>
   );
 }
