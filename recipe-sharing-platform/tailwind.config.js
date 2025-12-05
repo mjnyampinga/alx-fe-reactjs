7478f46b81fe8e4e98a52d75be1411e0c69bd010
nmjoselyne09 <nmjoselyne09@gmail.com>
2025-12-05 13:26:30 +0200

    Fix tailwind config for ALX validator

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html'
   ],
+  darkMode: false,
   theme: {
     extend: {},
   },
